e4c8b24df17bb72a497feaf2f82bda6f2c62b6aa
Pablo Romero <a21pablorf@iessanclemente.net>
2023-10-06 14:26:57 +0200

    6OCT

--- ProyectsAndStu/UD02_Actividad4/app/src/main/java/com/example/ud02_actividad4/MainActivity.kt
+++ ProyectsAndStu/UD02_Actividad4/app/src/main/java/com/example/ud02_actividad4/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.ud02_actividad4
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var salida=findViewById<TextView>(R.id.salida)
+        var boton=findViewById<Button>(R.id.boton)
+
+        boton.setOnClickListener { salida.setText("Lamando a juan perez") }
     }
 }
