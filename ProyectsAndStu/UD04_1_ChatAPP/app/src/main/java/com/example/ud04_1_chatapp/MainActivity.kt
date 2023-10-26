e795094869ea21324d5cbd1afcc1e1a690274f3e
Pablo Romero <a21pablorf@iessanclemente.net>
2023-10-26 14:27:36 +0200

    26OCT

--- ProyectsAndStu/UD04_1_ChatAPP/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
+++ ProyectsAndStu/UD04_1_ChatAPP/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
@@ -3,15 +3,32 @@ package com.example.ud04_1_chatapp
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var toolbar=findViewById<MaterialToolbar>(R.id.toolbar)
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        toolbar.setupWithNavController()
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController=findNavController(R.id.nav_host_fragment)
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item)
     }
 }
