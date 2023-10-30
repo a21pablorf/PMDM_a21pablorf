f5eb5ad523b9a9fb2c0d5575985ebcf91aba84d2
Pablo Romero <a21pablorf@iessanclemente.net>
2023-10-30 13:45:36 +0100

    30OC

--- ProyectsAndStu/UD04_1_ChatAPP/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
+++ ProyectsAndStu/UD04_1_ChatAPP/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,6 +42,9 @@ class MainActivity : AppCompatActivity() {
 
         //Panel Lateral
         val lateralPane = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navigationView=findViewById<NavigationView>(R.id.nav_view)
+
+
 
     }
 
